286551ffdada6e0a3f609e18b3cb6222dc29c48c
it06 <it06@kulanoil.kz>
2018-09-26 17:56:48 +0600

    animation

---
+++ 02_man_city/src/Components/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Feautred from "./features";
+
+const Home = () => {
+  return (
+    <div className="bck_blue">
+      <Feautred />
+    </div>
+  );
+};
+
+export default Home;
